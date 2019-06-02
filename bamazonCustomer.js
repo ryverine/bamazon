9a214cb2c1bcd6956ade56c192f83b4808696335
ryverine <rwcheek@gmail.com>
2019-06-01 21:48:34 -0400

    Using UUID as item_id. Not sure how this will go when I try to add to database from node.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,6 @@ var connection = mysql.createConnection(
 });
 
 
-
 connection.connect(function(err) 
 {
     if (err) throw err;
--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  item_id INT NOT NULL AUTO_INCREMENT,
+  item_id CHAR(36) NOT NULL,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) default 0,
@@ -11,16 +11,34 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
--- VALUES ("Nineteen Eighty-Four", "Books", "7.79", "100");
--- VALUES ("Snow Crash", "Books", "10.87", "50");
--- VALUES ("NieR: Automata", "Videogames", "27.48", "115");
--- VALUES ("Dark Souls III", "Videogames", "19.99", "10");
--- VALUES ("Masters of the Universe", "Movies", "11.99", "130");
--- VALUES ("The BeastMaster", "Movies", "11.29", "101");
--- VALUES ("HOLIFE 2 Slice Toaster", "Kitchen", "34.99", "1000");
--- VALUES ("Bodum Chambord French Press Coffee Maker", "Kitchen", "29.51", "236");
--- VALUES ("BIC Brite Liner Highlighter, Chisel Tip, Assorted Colors, 5-Count", "Office", "2.13", "427");
--- VALUES ("Post-it Notes, 3 x 3, 100-Sheet, Pack of 5", "Office", "9.29", "1020");
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+-- VALUES (uuid(), "Nineteen Eighty-Four", "Books", "7.79", "100");
+-- VALUES (uuid(), "Snow Crash", "Books", "10.87", "50");
+-- VALUES (uuid(), "NieR: Automata", "Videogames", "27.48", "115");
+-- VALUES (uuid(), "Dark Souls III", "Videogames", "19.99", "10");
+-- VALUES (uuid(), "Masters of the Universe", "Movies", "11.99", "130");
+-- VALUES (uuid(), "The BeastMaster", "Movies", "11.29", "101");
+-- VALUES (uuid(), "HOLIFE 2 Slice Toaster", "Kitchen", "34.99", "1000");
+-- VALUES (uuid(), "Bodum Chambord French Press Coffee Maker", "Kitchen", "29.51", "236");
+-- VALUES (uuid(), "BIC Brite Liner Highlighter, Chisel Tip, Assorted Colors, 5-Count", "Office", "2.13", "427");
+-- VALUES (uuid(), "Post-it Notes, 3 x 3, 100-Sheet, Pack of 5", "Office", "9.29", "1020");
 
 SELECT * FROM products
+
+-- UUID/GUID as item ID
+
+DELIMITER ;;
+CREATE TRIGGER before_insert_products
+BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+  IF new.item_id IS NULL THEN
+    SET new.item_id = uuid();
+  END IF;
+END
+;;
+
+
+
+
+
